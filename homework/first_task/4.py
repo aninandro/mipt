ef8da5a438710e13862302b8ba96b68c8d6b87f3
Nikolay Davydov <ndavydov0@192.168.1.8>
2026-09-05 11:12:13 +0300

    4th task with test data

---
+++ homework/first_task/4.py
@@ -0,0 +1,24 @@
+f = open("input.txt","r")
+arr = f.read().splitlines()
+numbers = arr[0]
+operation = arr[1]
+
+f.close()
+
+a = numbers.split(" ")
+s = int(a[0])
+
+if operation == "+":
+    for i in range(1,len(a)):
+        s += int(a[i])
+elif operation == "-":
+    for i in range(1, len(a)):
+        s -= int(a[i])
+elif operation == "*":
+    for i in range(1, len(a)):
+        s *= int(a[i])
+
+file = open("output.txt", "w")
+file.write(str(s) + "\n")
+
+    
