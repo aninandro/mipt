3979e6784e30ffcd749bdde7303a06f86beb32c4
Nikolay Davydov <nikolay.davydov2008@gmail.com>
2026-09-11 22:33:24 +0300

    fixed vowel sound logic error, examples from seminar file should be corrected

--- seminars/second_seminar/10.py
+++ seminars/second_seminar/10.py
@@ -1,12 +1,23 @@
-with open("input10.txt", "r") as f:
+with open("input11.txt", "r") as f:
     s = "".join("".join(f.readlines()).splitlines()).strip().lower().split()
     arr = []
+    vowels = {
+            "я" : "а",
+            "ю" : "у",
+            "ё" : "о",
+            "е" : "э",
+            "э" : "э",
+            "о" : "о",
+            "у" : "у",
+            "а" : "а",
+            "и" : "и"
+            }
     for word in s:
         new_word = ""
         for letter in range(len(word)):
             new_word += word[letter]
-            if new_word[-1] in "аяуюоёеэиы":
-                new_word += "с" + new_word[-1]
+            if new_word[-1] in "аяуюоёеэиы" and letter != 0 and word[letter-1] not in "аяуюоёеэиы":
+                new_word += "с" + vowels[new_word[-1]]
                 while letter < len(word) and word[letter] in "аяуюоёеэиы":
                     letter += 1
 
