b672ad39d3b103f727b7bd902956f25501b270ab
Nikolay Davydov <nikolay.davydov2008@gmail.com>
2026-09-12 09:37:37 +0300

    added input validation on n and card values: to validate duplicate values changed for loop with while loop

--- seminars/second_seminar/1.py
+++ seminars/second_seminar/1.py
@@ -1,11 +1,26 @@
 cards = []
-n = int(input("Enter number of cards: "))
+n = int(input("Enter number of cards n: "))
 if n > 54:
     print("Error, n exceeded 54")
+elif n < 1:
+    print("Error, n must be natural")
 else:
-    cards = [int(input(f"Enter {x+1}th card value: ")) for x in range(n-1)]
+    x = 0
+    while x < n - 1:
+        value = int(input(f"Enter {x+1}th card value: "))
+        if value in cards:
+            print("Error, value already entered\n")
+        elif value < 1:
+            print("Error, value must be positive\n")
+        elif value > n:
+            print("Error, value must not exceed the number of cards\n")
+        else:
+            cards.append(value)
+            x += 1
 
-for i in range(1, n + 1):
-    if i not in cards:
-        print(f"Missing card is: {i}")
-        break
+    #cards = [int(input(f"Enter {x+1}th card value: ")) for x in range(n-1)]
+
+    for i in range(1, n + 1):
+        if i not in cards:
+            print(f"\nMissing card is: {i}")
+            break
