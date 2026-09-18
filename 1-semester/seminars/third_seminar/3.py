de644fcfb654a031308cb320d5d82c01b494e90e
Nikolay <nikolay.davydov2008@gmail.com>
2026-09-18 23:10:47 +0300

    changed euclid algorythm logic and added out Acknowledgements: - Extended Euclidean Algorithm logic was explained to me by AI assistant(DeepSeek). - Every line of code was written by me.

--- 1-semester/seminars/third_seminar/3.py
+++ 1-semester/seminars/third_seminar/3.py
@@ -1,17 +1,20 @@
 
-
 def euclid(a, b):
     if b == 0:
-        return a
-    return euclid(b, a % b)
+        return 1, 0, a
+    x1, y1, d = euclid(b, a % b)
+
+    x = y1
+    y = x1 - (a // b) * y1
+    return x, y, d
 
 def main():
     string = input("Enter numbers: ")
     nums = string.split()
     a = int(nums[0])
     b = int(nums[1])
-
-    print(euclid(a,b))
-    return 0
+    result = euclid(a, b)
+    print(f"{result[0]} {result[1]} {result[2]}")
 
 main()
+
